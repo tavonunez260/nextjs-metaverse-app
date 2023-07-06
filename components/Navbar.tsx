4daf69a80295ff1ccf8f0581cf752efbe55f3cd1
Gustavo Nunez Ortiz <tavonunez260@gmail.com>
2023-07-06 14:29:12 -0500

    img changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,12 +6,18 @@ import Image from 'next/image'
 import styles from '@/styles'
 import { navVariants } from '@/utils'
 
+import moduleExports from '../next.config'
+
 export function Navbar() {
 	return (
 		<motion.nav animate="show" className={`${styles.xPaddings} py-8 relative`} initial="hidden" variants={navVariants}>
 			<div className="absolute w-[50%] inset-0 gradient-01" />
 			<div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-				<Image alt="search" className="object-contain" height={24} src="/search.svg" width={24} />
+				<img
+					alt="search"
+					className="w-[24px] h-[24px] object-contain"
+					src={`${moduleExports.assetPrefix || ''}/search.svg`}
+				/>
 				<h2 className="font-extrabold text-[24px] leading-[24px] text-white">METAVERSUS</h2>
 				<Image alt="menu" className="object-contain" height={24} src="/menu.svg" width={24} />
 			</div>
--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
 	output: 'export',
-	assetPrefix: process.env.NODE_ENV === 'production' ? '/nextjs-metaverse-app' : undefined,
-	basePath: process.env.NODE_ENV === 'production' ? '/nextjs-metaverse-app/' : '',
+	assetPrefix:
+		process.env.NODE_ENV === 'production' ? 'https://tavonunez260.github.io/nextjs-metaverse-app' : undefined,
+	basePath: process.env.NODE_ENV === 'production' ? 'https://tavonunez260.github.io/nextjs-metaverse-app/' : '',
 	reactStrictMode: true,
 	experimental: {
 		appDir: true
