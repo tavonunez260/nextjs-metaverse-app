8ac89a72ca0863b5e5433ba8ee4403e6601f47b5
Gustavo Nunez Ortiz <tavonunez260@gmail.com>
2023-07-06 12:33:54 -0500

    nextjs config changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
+	output: 'export',
+	reactStrictMode: true,
+	experimental: {
+		appDir: true
+	}
 }
 
 module.exports = nextConfig
